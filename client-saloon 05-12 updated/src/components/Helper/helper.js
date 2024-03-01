50dc9617242dddc2ee8f0d1d4a4f2c57770b432d
Laxmikasi <kasireddylaxmi66040@gmail.com>
2024-03-01 13:09:05 +0530

    hi

--- client-saloon 05-12 updated/src/components/Helper/helper.js
+++ client-saloon 05-12 updated/src/components/Helper/helper.js
@@ -1,2 +1,2 @@
-export const BASE_URL =  "https://saloon-server1.onrender.com"
-// export const BASE_URL =  "http://localhost:5000"
+// export const BASE_URL =  "https://saloon-server1.onrender.com"
+ export const BASE_URL =  "http://localhost:5000"
